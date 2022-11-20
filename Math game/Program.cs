a028f0cc654d67e04bffc7d063c13ac18dcdb113
MPRekrutacja <65944671+MPLekter@users.noreply.github.com>
2022-11-20 11:16:15 +0100

    changed output to interpolated strings

--- Math game/Program.cs
+++ Math game/Program.cs
@@ -4,14 +4,22 @@ namespace Math_game
 {
     class Program
     {
+
         static void Main(string[] args)
         {
+            int index = 1;
+            string name = "Pablo";
+            char initial = 'P';
+            int year = 2022;
+            double onethreethree = 1.33d;
+            bool loveToCode = true;
+
             Console.WriteLine("These are the datatypes:\n");
-            Console.WriteLine(1 + " -str, example: " + "Pablo");
-            Console.WriteLine(2 + " -int, example: " + 4);
-            Console.WriteLine(3 + " -bool, example: " + true);
-            Console.WriteLine(4 + " -char, example: " + 'P');
-            Console.WriteLine(5 + " -double, example: " + 1.33d);
+            Console.WriteLine($"{index++} -string, example: {name}");
+            Console.WriteLine($"{index++} -int, example: {year}");
+            Console.WriteLine($"{index++} -bool, example: {loveToCode}");
+            Console.WriteLine($"{index++} -char, example: {initial}");
+            Console.WriteLine($"{index++} -double, example: {onethreethree}");
             Console.ReadLine();
         }
     }
