0f943ea9d2c50624df4d9b6589463abc4e72f7aa
MPRekrutacja <65944671+MPLekter@users.noreply.github.com>
2022-11-20 11:28:42 +0100

    refactored code using $@ to form a single statement instead of multiple statements

--- Math game/Program.cs
+++ Math game/Program.cs
@@ -14,12 +14,24 @@ namespace Math_game
             double onethreethree = 1.33d;
             bool loveToCode = true;
 
+            //by using $@ in string, you can make bigger chunks of info in multiple lines without using multiple statements. Useful.
+            string myParagraph = $@"These are the datatypes: 
+            {index++} -string, example: {name}
+            {index++} -int, example: {year}
+            {index++} -bool, example: {loveToCode}
+            {index++} -char, example: {initial}
+            {index++} -double, example: {onethreethree}";
+
+            //works the same as below
+            /*
             Console.WriteLine("These are the datatypes:\n");
             Console.WriteLine($"{index++} -string, example: {name}");
             Console.WriteLine($"{index++} -int, example: {year}");
             Console.WriteLine($"{index++} -bool, example: {loveToCode}");
             Console.WriteLine($"{index++} -char, example: {initial}");
             Console.WriteLine($"{index++} -double, example: {onethreethree}");
+            */
+            Console.WriteLine(myParagraph);
             Console.ReadLine();
         }
     }
