f4d553d298f3e93d0aff89c2caf9959bc9c3b7fa
MPRekrutacja <65944671+MPLekter@users.noreply.github.com>
2022-11-20 13:23:16 +0100

    introducing branching - an if statement now determines the outcome.

--- Math game/Program.cs
+++ Math game/Program.cs
@@ -7,32 +7,71 @@ namespace Math_game
 
         static void Main(string[] args)
         {
-            int index = 1;
-            string name = "Pablo";
-            char initial = 'P';
-            int year = 2022;
-            double onethreethree = 1.33d;
-            bool loveToCode = true;
+            #region initial_lesson
+            /* int index = 1;
+             string name = "Pablo";
+             char initial = 'P';
+             int year = 2022;
+             double onethreethree = 1.33d;
+             bool loveToCode = true;
 
-            //by using $@ in string, you can make bigger chunks of info in multiple lines without using multiple statements. Useful.
-            string myParagraph = $@"These are the datatypes: 
-            {index++} -string, example: {name}
-            {index++} -int, example: {year}
-            {index++} -bool, example: {loveToCode}
-            {index++} -char, example: {initial}
-            {index++} -double, example: {onethreethree}";
 
-            //works the same as below
-            /*
-            Console.WriteLine("These are the datatypes:\n");
-            Console.WriteLine($"{index++} -string, example: {name}");
-            Console.WriteLine($"{index++} -int, example: {year}");
-            Console.WriteLine($"{index++} -bool, example: {loveToCode}");
-            Console.WriteLine($"{index++} -char, example: {initial}");
-            Console.WriteLine($"{index++} -double, example: {onethreethree}");
-            */
-            Console.WriteLine(myParagraph);
-            Console.ReadLine();
+             //by using $@ in string, you can make bigger chunks of info in multiple lines without using multiple statements. Useful.
+             string myParagraph = $@"These are the datatypes: 
+             {index++} -string, example: {name}
+             {index++} -int, example: {year}
+             {index++} -bool, example: {loveToCode}
+             {index++} -char, example: {initial}
+             {index++} -double, example: {onethreethree}";
+
+             //works the same as below
+
+             Console.WriteLine("These are the datatypes:\n");
+             Console.WriteLine($"{index++} -string, example: {name}");
+             Console.WriteLine($"{index++} -int, example: {year}");
+             Console.WriteLine($"{index++} -bool, example: {loveToCode}");
+             Console.WriteLine($"{index++} -char, example: {initial}");
+             Console.WriteLine($"{index++} -double, example: {onethreethree}");
+             */
+            //Console.WriteLine(myParagraph);
+            #endregion
+
+            Console.WriteLine("What is your name?");
+
+            var name = Console.ReadLine();
+            var date = DateTime.UtcNow;
+
+            Console.WriteLine("---------------------");
+            Console.WriteLine($"Hello {name.ToUpper()}. It's {date}. This is such a fun thing.\n");
+            Console.WriteLine($@"What game would you like to play today?
+            Choose:
+             A - Addition
+             S - Substraction
+             M- Multiplication
+             D - Division
+             Q - Quit the program");
+            Console.WriteLine("---------------------");
+
+            var gameSelected = Console.ReadLine();
+
+            //.Trim() is needed to remove whitespaces from before and after input.
+            if (gameSelected.Trim() == "a" || gameSelected.Trim() == "A") 
+            { Console.WriteLine("A selected");  }
+            else if (gameSelected.Trim() == "s" || gameSelected.Trim().Trim() == "S")
+            { Console.WriteLine("S selected"); }
+            else if (gameSelected.Trim() == "m" || gameSelected.Trim() == "M")
+            { Console.WriteLine("M selected"); }
+            else if (gameSelected.Trim() == "D" || gameSelected.Trim() == "D")
+            { Console.WriteLine("D selected"); }
+            else if (gameSelected.Trim() == "q" || gameSelected.Trim() == "Q")
+            { Environment.Exit(1); }
+            else
+            { 
+                Console.WriteLine("invalid input, try again");
+                Console.ReadLine();
+            }
+            
+
         }
     }
 }
