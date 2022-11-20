e711ceba065dd3a0db83a163b7f35f7d08cb219c
MPRekrutacja <65944671+MPLekter@users.noreply.github.com>
2022-11-20 10:59:25 +0100

    listed datatypes

--- Math game/Program.cs
+++ Math game/Program.cs
@@ -6,7 +6,13 @@ namespace Math_game
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("These are the datatypes:\n");
+            Console.WriteLine(1 + " -str, example: " + "Pablo");
+            Console.WriteLine(2 + " -int, example: " + 4);
+            Console.WriteLine(3 + " -bool, example: " + true);
+            Console.WriteLine(4 + " -char, example: " + 'P');
+            Console.WriteLine(5 + " -double, example: " + 1.33d);
+            Console.ReadLine();
         }
     }
 }
